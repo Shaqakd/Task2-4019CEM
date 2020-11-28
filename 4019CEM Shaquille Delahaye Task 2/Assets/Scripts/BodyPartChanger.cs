ed3f30792870dd67e7305674330ca6b2e4026cfc
Shaq <shaquilleDelahaye@outlook.com>
2020-11-28 17:17:06 +0000

    Task 2 project

---
+++ 4019CEM Shaquille Delahaye Task 2/Assets/Scripts/BodyPartChanger.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class BodyPartChanger : MonoBehaviour
+{
+    //Which sprite will be changed
+    public SpriteRenderer bodyPart;
+    //What list of sprites to change
+    public List<Sprite> Sprites = new List<Sprite>();
+    private int currentSprite = 0;
+    
+    public void SetSprite(int Num)
+    {
+        //Takes a integer value and updates the sprite array
+        if (Num > Sprites.Count)
+            return; 
+        
+        currentSprite = Num;
+        bodyPart.sprite = Sprites[currentSprite];
+    }
+
+    public void NextSprite()
+    {
+        //Will index once to the next value
+        currentSprite++;
+        if (currentSprite >= Sprites.Count)
+        {
+            currentSprite = 0;
+            //Resets the counter when all items in list have been cycled through
+        }
+        bodyPart.sprite = Sprites[currentSprite];
+    }
+    public void PreviousSprite()
+    {
+        //Will index once the previous value
+        currentSprite--;
+        if (currentSprite < 0)
+        {
+            currentSprite = Sprites.Count - 1;
+            //Rests when all objects in list have been cycled through
+        }
+        bodyPart.sprite = Sprites[currentSprite];
+    }
+    public void Randomizer()
+    {
+        //Randomizes the Sprite index
+        currentSprite = Random.Range(0, Sprites.Count - 1);
+        bodyPart.sprite = Sprites[currentSprite];
+    }
+
+}
---
+++ 4019CEM Shaquille Delahaye Task 2/Assets/Scripts/Menu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class Menu : MonoBehaviour
+{
+    public List<BodyPartChanger> bodyPartChangers = new List<BodyPartChanger>();
+    public TMP_InputField CDesign;
+
+    public void RandomizeCharacter()
+    {
+        // Runs through each bodypart and randomly selects one of the index sprite values
+        foreach (BodyPartChanger changer in bodyPartChangers)
+        {
+            changer.Randomizer();
+        }
+    }
+    public void Submit()
+    {
+        //fetchs each sprite name and prints the last character.
+        var Head = GameObject.Find("Head").GetComponent<SpriteRenderer>().sprite.name;
+        var Torso = GameObject.Find("Torso").GetComponent<SpriteRenderer>().sprite.name;
+        var LArm = GameObject.Find("LeftArm").GetComponent<SpriteRenderer>().sprite.name;
+        var Rarm = GameObject.Find("RightArm").GetComponent<SpriteRenderer>().sprite.name;
+        var LLeg = GameObject.Find("LeftLeg").GetComponent<SpriteRenderer>().sprite.name;
+        var RLeg = GameObject.Find("RightLeg").GetComponent<SpriteRenderer>().sprite.name;
+        Debug.Log(Head[Head.Length - 1].ToString() + Torso[Torso.Length - 1].ToString() + LArm[LArm.Length - 1].ToString()
+            + Rarm[Rarm.Length - 1].ToString() + LLeg[LLeg.Length - 1].ToString() + RLeg[RLeg.Length - 1].ToString());
+    }
+    public void Recreate()
+    {
+        //Reads the user input and Parses the string back to the index of the sprite
+        if (CDesign.text.Length == 6)
+        {
+            for (int i = 0; i < CDesign.text.Length; i++)
+            {
+                int SpriteNum = int.Parse(CDesign.text[i].ToString());
+                bodyPartChangers[i].SetSprite(SpriteNum-1);
+            }
+        }
+    }
+}
